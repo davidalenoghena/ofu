d49ee1d007c9796498edf3241ec3bf40b2688400
SkyCoder <emmanuelezenwigbo@gmail.com>
2020-08-11 11:39:55 +0100

    Home Page

---
+++ public/js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {    
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 10) {
+        $('.navbar').css('background','#151b25');
+        } else {
+        $('.navbar').css('background','transparent');
+        }
+    });
+});
