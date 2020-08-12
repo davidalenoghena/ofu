a28db1ca7477a7097bb481591e4874c45066fd52
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-08-12 14:45:19 +0100

    Just a little something

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,36 +1,23 @@
-@extends('layouts.home')
-@push('css')
+@extends('layouts.app')
 
-    <!--     Fonts and icons     -->
-
-    <link rel="stylesheet" type="text/css"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-    <link rel="stylesheet"
-        href="https://demos.creative-tim.com/material-dashboard/assets/css/material-dashboard.min.css?v=2.1.2">
-<script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
-<link href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.dataTables.min.css">
-    <link rel="stylesheet" href="{{ asset('css/dash-table.css') }}" />
-
-      <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css" />
-<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-    <link rel="stylesheet" href="{{asset('css/dash.css')}}">
-    <link rel="stylesheet" href="{{asset('css/jquery.toast.min.css')}}">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-
-@endpush
-<title>
-    OFU - Admin Dashboard
-</title>
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-    <div class="content">
-        <div class="container-fluid">
-            <div id="alert">
-                <h4>Underdevelopement</h4>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in as an admin! There is a difference') }}
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
