49c82e9e63625a6e27b90d1af4265db26e6f8bbf
SkyCoder <emmanuelezenwigbo@gmail.com>
2020-08-13 00:47:13 +0100

    Navbar UI Fix

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,22 +6,21 @@
                 <span class="two"></span>
                 <span class="three"></span>
         </button>
-            
-
-        <div class="container">
-        <div class="navbar-collapse collapse" id="navbarmenu">
-            <div class="navbar-brand">
+         
+        <div class = "navbanner">
+        <div class="navbar-brand ml">
             <span><strong>OFU<span class = "nav-span">||||</span></strong></span>
-            </div>
-
-            <div>
+        </div>
+        <div class="navbar-collapse collapse" id="navbarmenu">
                 <ul class="navbar-nav">
-                    <li class="nav-item dropdown ml">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">OPPORTUNITIES</a>
+                    <li class="nav-item dropdown ">
+                        <a class="nav-link dropdown-toggle text-white" href="#" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">OPPORTUNITIES</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown08">
-                        <a class="dropdown-item" href="#">SCHOLARSHIPS</a>
-                        <a class="dropdown-item" href="#">INTERNSHIPS</a>
-                        <a class="dropdown-item" href="#">GRANTS</a>
+                            <ul>
+                            <li class = "dropdown-li"><a class="dropdown-item" href="#">SCHOLARSHIPS</a></li>
+                            <li class = "dropdown-li"><a class="dropdown-item" href="#">INTERNSHIPS</a></li>
+                            <li class = "dropdown-li"><a class="dropdown-item" href="#">GRANTS</a></li>
+                            </ul>
                         </div>
                     </li>
                     <li class="nav-item active">
@@ -33,11 +32,15 @@
                     <li class="nav-item mr">
                         <a class="nav-link " href="#" >CONTACT</a>
                     </li>
-                    <li class = "nav-item"><a class = "nav-link" href="{{ route('login') }}">LOGIN</a></li>
-                    <li class = "nav-item btn-blue"><a class = "nav-link" href="{{ route('register') }}" >REGISTER</a></li>
+                </ul>
+        </div>
+</div>
+          
+            <div class = "ml-auto ">
+                <ul class = "navbar-nav login-btns">
+                    <li class = "nav-item"><a class = "transparent-btn px-3 nav-link" href="{{ route('login') }}">LOGIN</a></li>
+                    <li class = "nav-item register btn-blue"><a class = "nav-link" href="{{ route('register') }}" >REGISTER</a></li>
                 </ul>
             </div>
-        </div>
-        </div>
     </nav>
 </header>
