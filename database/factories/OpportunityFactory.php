7c12ff7a166ca455c1135b0e6d51c7b62646a7a7
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-09-09 21:18:25 +0100

    Opportunities schema

--- database/factories/OpportunityFactory.php
+++ database/factories/OpportunityFactory.php
@@ -7,6 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Opportunity::class, function (Faker $faker) {
     return [
-        //
+        'scholarshipname' => $faker->name,
+        'details' => $faker->name,
+        'phone' => $faker->randomNumber(3),
+        'email' => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'is_admin' => $faker->boolean()
+        //image?
+        //Eligibility?
     ];
 });
