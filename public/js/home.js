d7e9a8488106df5b6ab88fc3f37df1e1131e6d85
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-09-13 01:12:59 +0100

    Homepage Redesign

--- public/js/home.js
+++ public/js/home.js
@@ -5,10 +5,16 @@ $(document).ready(function() {
         $('#ofu-navbar').css('background','transparent');
         $('.switch').css('color', '#fff');
         $('.nav-grey').css('color', '#fff');
+        $('.one').css('background-color', '#fff');
+        $('.two').css('background-color', '#fff');
+        $('.three').css('background-color', '#fff');
         } else {
         $('#ofu-navbar').css('background','#E7ECFF');
         $('.switch').css('color', '#3C64B1');
         $('.nav-grey').css('color', '#373f41');
+        $('.one').css('background-color', '#3c64b1');
+        $('.two').css('background-color', '#3c64b1');
+        $('.three').css('background-color', '#3c64b1');
         }
     });
 });
