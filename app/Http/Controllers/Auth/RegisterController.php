280f1fb46aaca4c01d37040c9637f388228c800f
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-10-29 10:07:57 +0100

    navbar and auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::ADMIN;
+    //protected $redirectTo = RouteServiceProvider::ADMIN;
 
     /**
      * Create a new controller instance.
