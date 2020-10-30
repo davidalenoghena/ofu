4941f50bfa7f76c2f913847ac2a2f8903a53be08
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-10-30 18:17:39 +0100

    bug Fix

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,7 +20,7 @@
                         <a class="nav-link  switch" href="{{ route('home') }}">HOME</a>
                 </li>
                 <li class="nav-item dropdown ">
-                    <a href = "{{ route ('opportunities') }}" class="nav-link nav-grey dropdown-toggle " id="dropdown08" aria-haspopup="true" aria-expanded="false">OPPORTUNITIES</a>
+                    <span class="nav-link nav-grey dropdown-toggle " id="dropdown08" aria-haspopup="true" aria-expanded="false">OPPORTUNITIES</span>
                     <div class="dropdown-menu" aria-labelledby="dropdown08">
                         <ul>
                             <li class = "dropdown-li"><a class="dropdown-item"  href = "{{ route ('opportunities') }}" >SCHOLARSHIPS</a></li>
